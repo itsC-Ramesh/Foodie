dc387e75340e33083344421ea8bb91e87b5fe262
Ramesh Choudhary <rchoudhary@accoonaglobal.com>
2023-03-28 00:50:57 +0530

    Exception Handling with Filters

--- Foodie.Api/Program.cs
+++ Foodie.Api/Program.cs
@@ -1,3 +1,4 @@
+using Foodie.Api.Filters;
 using Foodie.Application;
 using Foodie.Infrastructure;
 
@@ -6,8 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
-        
-    builder.Services.AddControllers();
+
+    builder.Services.AddControllers(options => options.Filters.Add<ExceptionHandlingFilterAttribute>());
 }
 
 var app = builder.Build();
