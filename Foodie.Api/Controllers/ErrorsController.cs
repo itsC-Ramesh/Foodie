e41fc61ac1fba3ee71282b1c8d97a76dce2e2140
Ramesh Choudhary <rchoudhary@accoonaglobal.com>
2023-03-28 01:06:45 +0530

    Exceptio handling via error endpoint

---
+++ Foodie.Api/Controllers/ErrorsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foodie.Api.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class ErrorsController : ControllerBase
+    {
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            Exception? ex = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+            return Problem(
+                title: ex?.Message,
+                statusCode: StatusCodes.Status500InternalServerError
+                );
+        }
+    }
+}
--- Foodie.Api/Program.cs
+++ Foodie.Api/Program.cs
@@ -8,11 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
 
-    builder.Services.AddControllers(options => options.Filters.Add<ExceptionHandlingFilterAttribute>());
+    //builder.Services.AddControllers(options => options.Filters.Add<ExceptionHandlingFilterAttribute>());
+    builder.Services.AddControllers();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
+
     app.UseHttpsRedirection();
 
     app.MapControllers();
